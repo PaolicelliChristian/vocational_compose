78714cb10733a0c7bd8e7a7ace6454d0d1192c08
christian_paolicelli <christian.paolicelli@digistone.it>
2024-04-12 17:04:29 +0200

    OfferItem update

--- app/src/main/java/com/example/vocational_compose/components/OfferItem.kt
+++ app/src/main/java/com/example/vocational_compose/components/OfferItem.kt
@@ -4,9 +4,13 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.Divider
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
@@ -19,53 +23,100 @@ import com.example.vocational_compose.R
 @Preview(showBackground = true)
 @Composable
 fun OfferItem() {
-
     Box(
-        modifier = Modifier
-            .fillMaxSize()
+        Modifier
+            .fillMaxWidth()
     ) {
-        Row {
+        Column {
 
-            Column {
-                NormalText(
-                    text = "Offer.Title",
-                    fontSize = 14.sp,
-                    fontWeight = FontWeight.SemiBold,
-                    modifier = Modifier,
-                    textColor = colorResource(id = R.color.userBlue),
-                )
-                NormalText(
-                    text = "CompanyName",
-                    fontSize = 12.sp,
-                    fontWeight = FontWeight.Normal,
-                    modifier = Modifier,
-                    textColor = colorResource(id = R.color.grey)
-                )
-                NormalText(
-                    text = "Offer.InsertMode",
-                    fontSize = 12.sp,
-                    fontWeight = FontWeight.Normal,
+            Row(
+                verticalAlignment = Alignment.CenterVertically,
+                modifier = Modifier.fillMaxWidth()
+            ) {
+                Column(
+                    Modifier
+                        .fillMaxWidth()
+                        .weight(0.7f)
+                ) {
+                    NormalText(
+                        text = "Offer.Title",
+                        fontSize = 14.sp,
+                        fontWeight = FontWeight.SemiBold,
+                        modifier = Modifier.fillMaxWidth(),
+                        textColor = colorResource(id = R.color.userBlue),
+                    )
+                    NormalText(
+                        text = "CompanyName",
+                        fontSize = 12.sp,
+                        fontWeight = FontWeight.Normal,
+                        modifier = Modifier.fillMaxWidth(),
+                        textColor = colorResource(id = R.color.grey)
+                    )
+
+                    Row {
+                        Column(
+                            Modifier
+                                .fillMaxWidth()
+                                .weight(0.5f)
+                        ) {
+                            NormalText(
+                                text = "Offer.InsertMode",
+                                fontSize = 12.sp,
+                                fontWeight = FontWeight.Normal,
+                                modifier = Modifier
+                            )
+                            NormalText(
+                                text = "Location.City",
+                                fontSize = 12.sp,
+                                fontWeight = FontWeight.Normal,
+                                modifier = Modifier
+                            )
+                        }
+                        Column(
+                            Modifier
+                                .fillMaxWidth()
+                                .weight(0.5f)
+                        ) {
+                            NormalText(
+                                text = "offer.ImpegnoRichiest",
+                                fontSize = 12.sp,
+                                fontWeight = FontWeight.Normal,
+                                modifier = Modifier
+                            )
+                            NormalText(
+                                text = "offer.workPlae",
+                                fontSize = 12.sp,
+                                fontWeight = FontWeight.Normal,
+                                modifier = Modifier
+                            )
+                        }
+                    }
+                }
+
+                Image(
+                    painter = painterResource(id = R.drawable.logo_vt_landing),
+                    contentDescription = "Logo",
                     modifier = Modifier
+                        .fillMaxWidth()
+                        .weight(0.3f)
                 )
-                NormalText(
-                    text = "Location.City",
-                    fontSize = 12.sp,
-                    fontWeight = FontWeight.Normal,
-                    modifier = Modifier
-                )
+            }
+            Button(
+                onClick = { /*TODO*/ },
+                Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 10.dp),
+                shape = RoundedCornerShape(1.dp)
+            ) {
 
             }
 
-            Image(
-                painter = painterResource(id = R.drawable.logo_vt_landing),
-                contentDescription = "Logo",
-                modifier = Modifier
-//                    .padding(start = 40.dp, bottom = 20.dp)
-                    .size(width = 120.dp, height = 50.dp)
+            Divider(
+                thickness = 1.dp,
+                color = colorResource(id = R.color.deepBlue),
             )
-
-
         }
+
 
     }
 
